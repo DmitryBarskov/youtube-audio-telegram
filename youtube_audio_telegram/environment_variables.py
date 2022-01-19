162cb8d6e505b7ff636a02439f7d5eaa849a5f2f
Dmitry Barskov <dmitry.barskov@flatstack.com>
2022-01-19 08:54:26 +0300

    Change production.py

--- youtube_audio_telegram/environment_variables.py
+++ youtube_audio_telegram/environment_variables.py
@@ -6,6 +6,5 @@ ENV.read_env()
 
 TELEGRAM_API_TOKEN = ENV('TELEGRAM_API_TOKEN')
 PORT = ENV.int('PORT', 8443)
-print("PORT:", PORT)
 PY_ENV = ENV('PY_ENV', 'production')
 HOST = ENV('HOST', None)
--- youtube_audio_telegram/production.py
+++ youtube_audio_telegram/production.py
@@ -5,6 +5,7 @@ from environment_variables import (HOST, PORT, TELEGRAM_API_TOKEN)
 
 def start_bot(updater: Updater):
     updater.start_webhook(
-        listen="0.0.0.0", port=PORT, url_path=TELEGRAM_API_TOKEN
+        listen="0.0.0.0", port=PORT, url_path=TELEGRAM_API_TOKEN,
+        webhook_url=f"{HOST}/{TELEGRAM_API_TOKEN}"
     )
-    updater.bot.setWebhook(HOST + '/' + TELEGRAM_API_TOKEN)
+    updater.idle()
