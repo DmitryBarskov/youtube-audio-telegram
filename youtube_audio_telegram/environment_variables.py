9fa6bc77a922df2d51352a9514a927909e742ead
Dmitry Barskov <dmitry.barskov@flatstack.com>
2022-01-19 08:41:40 +0300

    default port 8443

--- youtube_audio_telegram/environment_variables.py
+++ youtube_audio_telegram/environment_variables.py
@@ -5,6 +5,6 @@ ENV = Env()
 ENV.read_env()
 
 TELEGRAM_API_TOKEN = ENV('TELEGRAM_API_TOKEN')
-PORT = ENV.int('PORT', 5000)
+PORT = ENV.int('PORT', 8443)
 PY_ENV = ENV('PY_ENV', 'production')
 HOST = ENV('HOST', None)
