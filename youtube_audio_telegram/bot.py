54690a82243107009befc790c2bf6f166490d71c
Dmitry Barskov <dmitry.barskov@flatstack.com>
2022-01-19 10:31:56 +0300

    Enable audio downloads

--- youtube_audio_telegram/bot.py
+++ youtube_audio_telegram/bot.py
@@ -1,19 +1,39 @@
 import __init__
 
+import re
+import io
+
+from pytube import YouTube
 from telegram import Update
 from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
 
 from environment_variables import (TELEGRAM_API_TOKEN, PORT)
 from __init__ import start_bot
 
-YT_PATTERN = r'(https?://)?(youtu\.be/\w{11}|(www\.)?youtube\.com/watch\?v=\w{11})'
+YT_PATTERN = r'((https?://)?(youtu\.be/\w{11}|(www\.)?youtube\.com/watch\?v=\w{11}))'
+
+
+def format_video_link(link: str) -> str:
+    return f"[This]({link}) is a youtube video"
 
 
 def youtube_video(update: Update, context: CallbackContext):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="This is a youtube video"
-    )
+    matches = re.findall(YT_PATTERN, update.message.text)
+
+    yt_links = map(lambda match: match[0], matches)
+    for link in yt_links:
+        buffer = io.BytesIO()
+        video = YouTube(link)
+        audio = video.streams.get_audio_only()
+        audio.stream_to_buffer(buffer)
+        context.bot.send_audio(
+            chat_id=update.effective_chat.id,
+            audio=buffer.getvalue(),
+            reply_to_message_id=update.message.message_id,
+            duration=video.length,
+            performer=video.author,
+            title=video.title
+        )
 
 
 def start(update: Update, context: CallbackContext):
